ec1c3fb15e87b2cde00d3a222a141d7befda81e8
Thanin Wangveerathananon <THANINWA@cementhai.com>
2020-07-15 18:14:53 +0700

    Fixed Raw Cell

--- woodpricing.py
+++ woodpricing.py
@@ -1,26 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import plotly.express as px
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-
-from jupyter_dash import JupyterDash
-app = JupyterDash(__name__)
-# In[ ]:
-
-
 import dash
 app = dash.Dash(__name__)
-
-
-# In[3]:
-
-
 import pandas as pd; import numpy as np; wpg=pd.read_csv('wpg.csv')
 from sklearn.preprocessing import StandardScaler
 scaler,wpgs = {},{}
@@ -34,10 +13,6 @@ for c in wpg.Channel.unique():
     wpgs[c][['Baht','Ton']] = scaler[c].transform(wps)
     wpgs[c]=pd.melt(wpgs[c], id_vars=['Zone','Channel','Year','Month'],var_name='Type', value_name='Value')
 
-
-# In[8]:
-
-
 # Build App
 app.layout = html.Div([
     html.H1("Wood Pricing Strategy"),
@@ -50,10 +25,6 @@ app.layout = html.Div([
     dcc.Graph(id='graph1'),
 ])
 
-
-# In[5]:
-
-
 # Define callback to update graph
 @app.callback(
     Output('graph', 'figure'),
@@ -62,11 +33,8 @@ app.layout = html.Div([
 def update_figure(c):
     return px.line(wpgs[c], x='Month', y='Value', facet_row='Type', color='Year')
 
-# Run app and display result inline in the notebook
-app.run_server(mode='external')
-# In[ ]:
+
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
