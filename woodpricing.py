5870e70b45a3b49deb82b344abe466dc2ff91725
dash.kim <me@thanin.kim>
2020-07-19 21:35:33 +0000

    Prepare Notebook

--- woodpricing.py
+++ woodpricing.py
@@ -11,7 +11,10 @@ app = dash.Dash(__name__)
 server = app.server
 
 scaler, wpgs = {}, {}
-wpg=pd.read_csv('wpg.csv')
+wpg = pd.read_csv('wpg.csv')
+
+
+
 for c in wpg.Channel.unique():
     scaler[c] = {}
     scaler[c] = StandardScaler()
@@ -19,7 +22,8 @@ for c in wpg.Channel.unique():
     wps = wpgs[c][['Baht', 'Ton']]
     scaler[c].fit(wps)
     wpgs[c][['Baht', 'Ton']] = scaler[c].transform(wps)
-    wpgs[c]=pd.melt(wpgs[c], id_vars=['Zone', 'Channel', 'Year', 'Month'], var_name='Type', value_name='Value')
+    wpgs[c] = pd.melt(wpgs[c], var_name='Type', value_name='Value',
+                      id_vars=['Zone', 'Channel', 'Year', 'Month'])
 
 # Build App
 app.layout = html.Div([
@@ -27,11 +31,12 @@ app.layout = html.Div([
     html.Label(["channel", dcc.Dropdown(
         id='channel-dropdown', clearable=False, value='N3NB',
         options=[{'label': c1, 'value': c1} for c1 in wpg.Channel.unique()])
-               ]),
+                ]),
     dcc.Graph(id='graph'),
     html.H2("More Graph Comming Soon..."),
     dcc.Graph(id='graph1'),
 ])
+
 
 # Define callback to update graph
 @app.callback(
@@ -41,5 +46,8 @@ app.layout = html.Div([
 def update_figure(c):
     return px.line(wpgs[c], x='Month', y='Value', facet_row='Type', color='Year')
 
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    # app.run_server(port=80,debug=True)
+    app.run_server(port=80, host='0.0.0.0')
+    # waitress-serve --listen="0.0.0.0:80" woodpricing:server
